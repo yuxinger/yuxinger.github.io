b35015b151b5bc6436ec488a100e7035a8d2e31f
yuxinger <113181736+yuxinger@users.noreply.github.com>
2022-12-28 14:43:04 +0800

    Add files via upload

---
+++ txx/uu/uu/js/SearchX.js
@@ -0,0 +1,13 @@
+
+var SearchX = {};
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
